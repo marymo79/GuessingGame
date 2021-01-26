b894b58fa59b37497764a69ff14250e986cc4db2
unknown <cuevastl@gmail.com>
2021-01-26 15:22:29 -0500

    Commented out our third guess, removed loop, added guess feedback.

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,69 +10,78 @@ namespace GuessingGame
             Console.WriteLine("Pick a number between 1 and 10");
             Console.WriteLine("If you want to leave the game, enter -1");
             int userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            Random rd = new Random();
+            int magicNumber = rd.Next(1, 10);
 
-            if (userNumber == 7)
+            if (userNumber == magicNumber)
             {
                 Console.WriteLine("You Won!!");
             }
             else if (userNumber == 0)
             {
                 Console.WriteLine("0 is not a number between 1 and 10, loser");
-                            }
+            }
             else if (userNumber == -1)
             {
                 Console.WriteLine("End Game");
             }
             else
             {
-                Console.WriteLine("Guess again");
-                userNumber = Convert.ToInt32(Console.ReadLine());
-                while (userNumber != 7)
+                if (userNumber > magicNumber)
                 {
-                    
-                    if (userNumber == 7)
-                    {
-                        Console.WriteLine("You Won!!");
-                    }
-                    else if (userNumber == 0)
-                    {
-                        Console.WriteLine("0 is not a number between 1 and 10, loser");
-                        break;
-                    }
-                    else if (userNumber == -1)
-                    {
-                        Console.WriteLine("End Game");
-                        break;
-                    }
-                    else
-                    {
-                        while (userNumber != 7)
-                        {
-                            Console.WriteLine("Guess again");
-                            userNumber = Convert.ToInt32(Console.ReadLine());
-
-                            if (userNumber == 7)
-                            {
-                                Console.WriteLine("You Won!!");
-                            }
-                            else if (userNumber == 0)
-                            {
-                                Console.WriteLine("0 is not a number between 1 and 10, loser");
-                                // this will repeat line in first loop 
-                                break;
-                                                            }
-                            else if (userNumber == -1)
-                            {
-                                Console.WriteLine("Game Over");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You are out guesses! GAME OVER!");
-                            }
-                            
-                        }
-                    }
+                    Console.WriteLine("Your guess was too big.");
                 }
+                else
+                {
+                    Console.WriteLine("Your guess was too small.");
+                }
+                
+                Console.WriteLine("Guess again.");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+                
+                if (userNumber == magicNumber)
+                {
+                    Console.WriteLine("You Won!!");
+                }
+                else if (userNumber == 0)
+                {
+                    Console.WriteLine("0 is not a number between 1 and 10, loser");
+                }
+                else if (userNumber == -1)
+                {
+                    Console.WriteLine("End Game");
+                }
+                else
+                {
+                    Console.WriteLine("You are out guesses! GAME OVER!");
+                        //while (userNumber != 7)
+                        //{                                                                                                               
+                        //    Console.WriteLine("Guess again");
+                        //    userNumber = Convert.ToInt32(Console.ReadLine());
+
+                        //    if (userNumber == 7)
+                        //    {
+                        //        Console.WriteLine("You Won!!");
+                        //    }
+                        //    else if (userNumber == 0)
+                        //    {
+                        //        Console.WriteLine("0 is not a number between 1 and 10, loser");
+                        //        // this will repeat line in first loop 
+                        //        break;
+                        //                                    }
+                        //    else if (userNumber == -1)
+                        //    {
+                        //        Console.WriteLine("Game Over");
+                        //    }
+                        //    else
+                        //    {
+                        //        Console.WriteLine("You are out guesses! GAME OVER!");
+                        //    }
+                            
+                        //}
+                }
+                
             }
         }
     }
